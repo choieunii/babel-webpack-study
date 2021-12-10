0e18242571dae6b2c78887c0ea4947d8d1cde702
choieunii <ghk784@dgu.ac.kr>
2021-12-11 02:27:11 +0900

    플러그인 사용하기

--- webpack-plugin/webpack.config.js
+++ webpack-plugin/webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react'],
+            presets: ["@babel/preset-react"],
           },
         },
       },
@@ -25,16 +25,16 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './template/index.html',
+      template: "./template/index.html",
     }),
     new webpack.DefinePlugin({
       APP_VERSION: '"1.2.3"', // 또는 JSON.stringify('1.2.3'),
-      TEN: '10',
+      TEN: "10",
     }),
     new webpack.ProvidePlugin({
-      React: 'react',
-      $: 'jquery',
+      React: "react",
+      $: "jquery",
     }),
   ],
-  mode: 'production',
+  mode: "production",
 };
